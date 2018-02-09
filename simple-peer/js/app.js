d9537c9fada8d3e3f058c333af653ec9ebc8ac39
Hector Zelaya <inge.zelaya@gmail.com>
2018-02-09 17:02:13 -0600

    removed styles and pubnub keys

--- simple-peer/js/app.js
+++ simple-peer/js/app.js
@@ -1,58 +1,106 @@
 var inputCallee = document.getElementById('callee')
 var btnCall = document.getElementById('btnCall')
 var btnHangup = document.getElementById('btnHangup')
+var inputCaller = document.getElementById('caller')
+var btnLogin = document.getElementById('btnLogin')
+var showUser = document.getElementById('showUser')
 var localVideo = document.getElementById('localVideo')
 var remoteVideo = document.getElementById('remoteVideo')
 
 var p
 const pubnub = new PubNub({
-  publishKey: 'pub-c-17febd84-2edf-456d-9ab0-72bba81bfd79',
-  subscribeKey: 'sub-c-c4a8d352-e17f-11e6-884c-0619f8945a4f'
+  publishKey: 'your keys here',
+  subscribeKey: 'your keys here'
+})
+
+btnLogin.addEventListener('click', (evt, btn) => {
+  showUser.innerText = 'logged in as ' + inputCaller.value
+
+  pubnub.subscribe({
+    channels: [inputCaller.value]
+  })
+
+  pubnub.addListener({
+    message: message => {
+      console.log('received', message)
+      if (!p) {
+        createPeer(false, message.message.caller, () => {
+          console.log('answering ', message.message.caller)
+          p.signal(message.message.data)
+        })
+      } else {
+        p.signal(message.message.data)
+      }
+    }
+  })
+
+  caller.style.display = 'none'
+  btnLogin.style.display = 'none'
+  showUser.style.display = 'inline'
+  inputCallee.style.display = 'inline'
+  btnCall.style.display = 'inline'
 })
 
 btnCall.addEventListener('click', (evt, btn) => {
-  
-  pubnub.subscribe({
-    channels: [inputCallee.value]
-  });
+  createPeer(true, inputCallee.value, () => {
+    console.log('calling ', inputCallee)
+  })
+})
 
-  navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-    .then(stream => {
-      localvideo.src = window.URL.createObjectURL(stream)
+btnHangup.addEventListener('click', (evt, btn) => {
+  inputCallee.style.display = 'inline'
+  btnCall.style.display = 'inline'
+  btnHangup.style.display = 'none'
 
-      p = new SimplePeer({ initiator: true, stream: stream })
+  p.destroy();
+  p = null;
 
-      p.on('signal', data => {
-        pubnub.publish({
-          channel: inputCallee.value,
-          message: data
-        })
-      })
+  localVideo.src = '';
+  remoteVideo.src = '';
+})
 
-      p.on('stream', function (stream) {
-        // got remote video stream, now let's show it in a video tag
-        remotevideo.src = window.URL.createObjectURL(stream)
+function createPeer(isInitiator, otherPeer, callback) {
+  navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: true
+  }).then(stream => {
+    inputCallee.style.display = 'none'
+    btnCall.style.display = 'none'
+    btnHangup.style.display = 'inline'
+
+    localVideo.src = window.URL.createObjectURL(stream)
+
+    p = new SimplePeer({
+      initiator: isInitiator,
+      stream: stream,
+      trickle: false
+    })
+
+    p.on('signal', data => {
+      console.log('sending:', JSON.stringify(data))
+      pubnub.publish({
+        channel: otherPeer,
+        message: {
+          caller: inputCaller.value,
+          data: JSON.stringify(data)
+        }
       })
     })
-})
 
-//p.on('error', function (err) { console.log('error', err) })
+    p.on('stream', function (stream) {
+      // got remote video stream, now let's show it in a video tag
+      remoteVideo.src = window.URL.createObjectURL(stream)
+    })
 
-/*p.on('signal', function (data) {
-  console.log('SIGNAL', JSON.stringify(data))
-  document.querySelector('#outgoing').textContent = JSON.stringify(data)
-}) */
+    p.on('close', () => {
+      inputCallee.style.display = 'inline'
+      btnCall.style.display = 'inline'
+      btnHangup.style.display = 'none'
+      p = null;
+      localVideo.src = '';
+      remoteVideo.src = '';
+    })
 
-/* document.querySelector('form').addEventListener('submit', function (ev) {
-  ev.preventDefault()
-  p.signal(JSON.parse(document.querySelector('#incoming').value))
-})
-
-p.on('connect', function () {
-  console.log('CONNECT')
-  p.send('whatever' + Math.random())
-})
-
-p.on('data', function (data) {
-  console.log('data: ' + data)
-}) */
+    callback()
+  })
+}
